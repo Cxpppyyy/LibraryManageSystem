a9ff485077c81db899d975ff5225e25ef4c91298
PangYang <summerrain2587@gmail.com>
2024-01-10 01:30:57 +0800

    第二次提交

--- src/main/java/com/example/library/Controller/RegisterController.java
+++ src/main/java/com/example/library/Controller/RegisterController.java
@@ -2,10 +2,11 @@ package com.example.library.Controller;
 
 import com.example.library.Service.RegisterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin(origins = "*",maxAge = 3600)
 @RestController
 @RequestMapping("/api")
 public class RegisterController {
--- src/main/java/com/example/library/DAO/RegisterDao.java
+++ src/main/java/com/example/library/DAO/RegisterDao.java
@@ -9,7 +9,7 @@ public class RegisterDao {
     @Autowired
     private JdbcTemplate jdbcTemplate;
     public void addUser(String username,String password,String email){
-        String sql = "INSERT INTO users(login_name,login_password,login_email) values(?,?,?)";
+        String sql = "INSERT INTO login(login_name,login_password,login_email) values(?,?,?)";
         jdbcTemplate.update(sql,username,password,email);
     }
 }
